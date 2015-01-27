48c4f68ff4a1dc845a4415228328679f132b1c03
Jason Hogrefe <hogrefejason@gmail.com>
2015-01-26 19:09:18 -0600

    Commit 5: Got non-tip code to work. Now need to add tip code.

--- checksplitter.rb
+++ checksplitter.rb
@@ -2,26 +2,21 @@ require "pry"
 
 class Checksplitter
   def initialize(tmcost, numgroup)
-    @total_meal_cost = tmcost1
-    @tip1 = 0.15
-    @tip2 = customtip
-    @number_in_group = numgroup    
+    @total_meal_cost = tmcost
+    @number_in_group = numgroup
   end
 
-  def get_meal_cost
+  def get_meal_cost()
     @total_meal_cost
   end
 
-  def get_number_in_group
+  def get_number_in_group()
     @number_in_group
   end
 
   def split_check
-    split = @total_meal_cost / @number_in_group
+    "The split is #{@total_meal_cost / @number_in_group} per person."
   end
-
 end
-
-
 
 binding.pry
