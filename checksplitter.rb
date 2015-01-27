1b581c3819443bac993aac98e2decca2a998e4bd
Jason Hogrefe <hogrefejason@gmail.com>
2015-01-26 22:55:59 -0600

    Commit 6: Everything working except for custom tip conversion.

--- checksplitter.rb
+++ checksplitter.rb
@@ -17,6 +17,18 @@ class Checksplitter
   def split_check
     "The split is #{@total_meal_cost / @number_in_group} per person."
   end
+  
+  def give_tip_standard
+    puts "Standard tip is 15 percent."
+    puts "The tip would be #{(@total_meal_cost * 0.15)}."
+  end
+  
+  def give_tip_custom
+    puts "Enter tip amount: "
+    answer = gets.chomp.to_i
+    custom_tip = "#{@total_meal_cost} * (#{answer} * 0.01)}"
+    puts "The custom tip is #{custom_tip}."
+  end
 end
 
 binding.pry
