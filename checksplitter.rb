c88bd5e4f115f771f1d61fa9a9adaf1e2cea8bbf
Jason Hogrefe <hogrefejason@gmail.com>
2015-01-26 18:01:26 -0600

    Commit 2: Defined attributes in checksplitter.

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,9 +1,20 @@
 class Checksplitter
-  def initialize
+  def initialize(tmcost, numgroup, customtip)
     @total_meal_cost = tmcost1
     @tip1 = tmcost * 0.15
     @tip2 = customtip
     @number_in_group = numgroup    
   end
-end
 
+  def get_meal_cost
+    @total_meal_cost
+  end
+
+  def get_number_in_group
+    @number_in_group
+  end
+
+  def get_custom_tip
+    @tip2
+  end
+end
