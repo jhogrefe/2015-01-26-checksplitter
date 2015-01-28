ba48d1476b6e8d3e799fafc231c2c0bb08706c40
Jason Hogrefe <hogrefejason@gmail.com>
2015-01-27 22:33:43 -0600

    Updated Checksplitter class with cleaner code and comments.

--- checksplitter.rb
+++ checksplitter.rb
@@ -1,73 +1,111 @@
 require "pry"
 
-class DinnerClub
-  attr_accessor :members, :owed
-  
-  def initialize(params = {})
-    @members = params[:members]
-    @owed = params[:owed]
-  end
-  
-  def get_amount
-    @owed
-  end
-  
-  # Add new method to push new amount member owes
-  # and add it to previous amount
-  def add_dinner(members, owed)
-    @owed.push(DinnerClub.new(member, owed))
-  end
-    
-end
 
+# Public: #Checksplitter
+# A class to determine how much each person in a group
+# owes based on the total bill, and how much tip to give
+# (both standard 15% and a custom tip amount).
+#
+# Params:
+# total_meal_cost - total_cost: gets total cost of meal.
+# number_in_group - num_in_group: gets number of people
+#                   in group.
+#
+# Returns:
+# split_check:       returns how much each person in the group
+#                    should pay.
+# give_tip_standard: returns how much tip is owed (15%).
+# give_tip_custom:   asks for custom amount and returns how
+#                    much tip is owed.
+#
+# State Changes:
+# None.
 
 class Checksplitter
-  
-  def initialize(tmcost, numgroup)
-    @total_meal_cost = tmcost
-    @number_in_group = numgroup
-  end
 
-  def get_meal_cost()
-    @total_meal_cost
-  end
-
-  def get_number_in_group()
-    @number_in_group
+  attr_accessor :total_meal_cost, :number_in_group
+  
+  # Public: #initialize
+  # Initializes the @total_meal_cost and @number_in_group
+  # attributes in the Checksplitter class.
+  #
+  # Params:
+  # total_cost   - total cost of meal.
+  # num_in_group - number of people in the group.
+  #
+  # Returns:
+  # None.
+  #
+  # State Changes:
+  # None.
+  
+  def initialize(total_cost, num_in_group)
+    @total_meal_cost = total_cost
+    @number_in_group = num_in_group
   end
   
-  # Add new method to push new amount member owes
-  # and add it to previous amount
-  def add_dinner(members, owed)
-    @owed.push(DinnerClub.new(members, owed))
-  end
+  # Public: #split_check
+  # A method that divides the total cost of the meal
+  # by the number of people in the group.
+  #
+  # Params:
+  # @total_meal_cost - value of the total meal cost.
+  # @number_in_group - value of the number of people in
+  #                    the group.
+  #
+  # Returns:
+  # Integer: the amount each person owes for the meal.
+  #
+  # State Changes:
+  # None.
   
-  # Add new method to total amount owed, 
-  # returning new total
-  def historical_amount
-    historical_amount = 0.0
-    @owed.each do |owed|
-      historical_amount += owed.get_amount
-    end
-    historical_amount
-  end
-
   def split_check
-    "The split is #{@total_meal_cost / @number_in_group} per person."
+    @total_meal_cost / @number_in_group
   end
+  
+  # Public: #give_tip_standard
+  # A method that multiplies the total cost of the meal
+  # by the float value of 15%.
+  #
+  # Params:
+  # @total_meal_cost - value of the total meal cost.
+  # 0.15             - value of standard tip of 15%.
+  #
+  # Returns:
+  # Integer: the amount owed for the tip for the 
+  #          standard tip value of 15%.
+  #
+  # State Changes:
+  # None.  
   
   def give_tip_standard
-    puts "Standard tip is 15 percent."
-    puts "The tip would be #{(@total_meal_cost * 0.15)}."
+    @total_meal_cost * 0.15
   end
+
+  # Public: #give_tip_custom
+  # A method that asks for a custom tip value and then
+  # multiplies the total cost of the meal by that value
+  # when converted to a float.
+  #
+  # Params:
+  # answer           - gets the custom tip amount and converts the
+  #                    value to an integer.
+  # @total_meal_cost - value of the total meal cost.
+  # 0.01             - convert percentage integer to float.
+  #
+  # Returns:
+  # Integer: the amount owed for the tip for the 
+  #          custom tip value.
+  #
+  # State Changes:
+  # None.  
   
   def give_tip_custom
     puts "Enter tip amount: "
     answer = gets.chomp.to_i
-    custom_tip = "#{(@total_meal_cost * answer * 0.01)}"
-    puts "The custom tip is #{custom_tip}."
+    @total_meal_cost * answer * 0.01
   end
-  
+
 end
 
 binding.pry
